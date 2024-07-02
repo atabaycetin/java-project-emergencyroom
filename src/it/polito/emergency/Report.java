12896af16b07b821d169e52d405f574068a88c86
Marco Torchiano <marco.torchiano@polito.it>
2024-07-02 12:16:19 +0200

    Initial version

---
+++ src/it/polito/emergency/Report.java
@@ -0,0 +1,25 @@
+package it.polito.emergency;
+
+public class Report {
+
+    public String getId() {
+        return null;
+    }
+
+    public String getProfessionalId() {
+        return null;
+    }
+
+    public String getFiscalCode() {
+        return null;
+    }
+
+    public String getDate() {
+        return null;
+    }
+
+
+    public String getDescription() {
+        return null;
+    }
+}
