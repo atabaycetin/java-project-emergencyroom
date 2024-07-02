fd3a31d522151bb7e4d6d601ffc906f2ffc3e609
Marco Torchiano <marco.torchiano@polito.it>
2024-07-02 12:16:19 +0200

    professional class constructor and getters implemented

--- src/it/polito/emergency/Professional.java
+++ src/it/polito/emergency/Professional.java
@@ -1,5 +1,6 @@
 package it.polito.emergency;
 
+<<<<<<< HEAD
 public class Professional {
 
     public String getId() {
@@ -24,5 +25,43 @@ public class Professional {
 
     public String getWorkingHours() {
         return null;
+=======
+import java.time.*;
+import java.time.format.*;
+
+public class Professional {
+
+    String id, name, surname, specialization, period;
+    LocalDate start, end;
+    public Professional (String id, String name, String surname, String specialization, String period) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.specialization = specialization;
+        this.period = period;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public String getWorkingHours() {
+        return "24/7";
+>>>>>>> df86daa (professional class constructor and getters implemented)
     }
 }
