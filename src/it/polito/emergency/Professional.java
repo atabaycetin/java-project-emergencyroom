615ab4490eb79af0742b47c2bad6890b1c26751e
crownlabs <student@crownlabs.polito.it>
2024-07-03 08:19:19 +0000

    startDate and endDate implemented

--- src/it/polito/emergency/Professional.java
+++ src/it/polito/emergency/Professional.java
@@ -1,15 +1,20 @@
 package it.polito.emergency;
 
+import java.time.*;
+
 public class Professional {
 
     String id, name, surname, specialization, period;
+    LocalDate start, end;
     public Professional (String id, String name, String surname, String specialization, String period) {
+        String[] temp = period.split(" to ");
+        start = LocalDate.parse(temp[0]);
+        end = LocalDate.parse(temp[1]);
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.specialization = specialization;
         this.period = period;
-        
     }
     public String getId() {
         return id;
@@ -34,4 +39,10 @@ public class Professional {
     public String getWorkingHours() {
         return "24/7";
     }
+	public LocalDate getStart() {
+		return start;
+	}
+	public LocalDate getEnd() {
+		return end;
+	}
 }
