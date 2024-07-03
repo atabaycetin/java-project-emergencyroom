bd4e1ce5b28f0213f4cb54ac25b3bb555abdd2c3
crownlabs <student@crownlabs.polito.it>
2024-07-03 09:47:55 +0000

    constructor and getters implemented

--- src/it/polito/emergency/Report.java
+++ src/it/polito/emergency/Report.java
@@ -1,25 +1,33 @@
 package it.polito.emergency;
 
 public class Report {
+    String reportId, professionalId, fiscalCode, date, description;
+    
+    public Report(String reportId, String professionalId, String fiscalCode, String date, String description) {
+		this.reportId = reportId;
+        this.professionalId = professionalId;
+		this.fiscalCode = fiscalCode;
+		this.date = date;
+		this.description = description;
+	}
 
-    public String getId() {
-        return null;
+	public String getId() {
+        return reportId;
     }
 
     public String getProfessionalId() {
-        return null;
+        return professionalId;
     }
 
     public String getFiscalCode() {
-        return null;
+        return fiscalCode;
     }
 
     public String getDate() {
-        return null;
+        return date;
     }
 
-
     public String getDescription() {
-        return null;
+        return description;
     }
 }
